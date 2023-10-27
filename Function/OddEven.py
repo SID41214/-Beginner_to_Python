ecdc5bd42dd489a8e2285571943d8ea84def95c8
Aboobacker Sidheeque <sidhub41214@gmail.com>
2023-10-27 15:05:59 +0530

    updated

---
+++ Function/OddEven.py
@@ -0,0 +1,13 @@
+def check(n):
+    if(n%2 ==0):
+        print(f"{n} is an Even Number")
+    else:
+        print(f"{n} is an  Odd Number")
+
+
+check(10)
+check(5)
+check(5654568413154656434654)
+
+        
+    
