ebb18e954044084f3a946f14353cd1de465529a1
Aboobacker Sidheeque <sidhub41214@gmail.com>
2023-10-30 11:14:27 +0530

    updated

---
+++ global_local_variable.py
@@ -0,0 +1,9 @@
+def f():
+	
+	# local variable
+	s = "I love Geeksforgeeks"
+	print("Inside Function:", s)
+
+# Driver code
+f()
+#print(s)     # this outside the function and return error for print(s) as notdefined
