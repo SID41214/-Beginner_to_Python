0107777a69f16ebc2d667c38f1b271deff89d974
Aboobacker Sidheeque <sidhub41214@gmail.com>
2023-11-01 15:32:43 +0530

    updated

---
+++ Problem solving/var.py
@@ -0,0 +1,28 @@
+# understanding type and variable
+name ="hello"
+c= name +" dil"
+print(c)
+
+
+# int & float
+x = 5
+y = 3.5
+result = x + y  # Result is 8.5 (a float)
+print(result)
+
+name ="rocksandsand"
+print(name[0])
+print(name[-1])
+print(name[0:2])
+print(name[1:4])
+print(name[3:])
+s= " my "+ name[4:8]
+print( "length of the string : " +  str(len(s)))
+
+
+#print(name[8]) will be index error
+
+
+#asd=print("hi") # type is None 
+#ef=asd + " good morning" # type None + string
+#print(ef)  # cannot concatenate 2 datatypes
