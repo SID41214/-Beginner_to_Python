16f9a535a9fba0b3415d79f8070b5d4cd095b753
Aboobacker Sidheeque <sidhub41214@gmail.com>
2023-11-02 16:14:51 +0530

    updated

---
+++ Problem solving/swapNumber.py
@@ -0,0 +1,16 @@
+a=int(input("enter value of a : "))
+b=int(input("enter value of b : "))
+temp =a
+a = b
+b = temp
+print (f"after swapping the values are:\n a= {a} \n b ={b}")
+
+print("*****************************************")
+x = int(input("Enter value of x: "))
+y = int(input("Enter value of y: "))
+
+x = x + y
+y = x - y
+x = x - y
+
+print(f"After swapping, the values are:\n x = {x}\n y = {y}")
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
-msg ="hello world"
-print(msg)
-msg.lower()
+#print("hello " + input("whats ur name:"))
+#print("hey "+input("ur name:")+",how are you")
+#name =len(input("whats your name"))
+#print(name)
