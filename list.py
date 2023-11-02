a5663f01c48904828ecaf514faa51bd810354bff
Aboobacker Sidheeque <sidhub41214@gmail.com>
2023-11-02 17:58:43 +0530

    updated

---
+++ list.py
@@ -0,0 +1,7 @@
+num=[25,484,45,87,7]
+a=sum(num)
+b=max(num)
+c=min(num)
+print(f"sum = {a}")
+print(f"max = {b}")
+print(f"min = {c}")
---
+++ tuple.py
@@ -0,0 +1,5 @@
+tup = (21,26,36,25,1,3,3,3,5)
+#print( tup[3] )
+a=tup.count(3)
+print(a)
+#it is ordered immutable collection of elemts
