ef4db371cdfce5a0ecf79e1537c9f3797d173cb0
Aboobacker Sidheeque <sidhub41214@gmail.com>
2023-11-03 16:01:11 +0530

    updated

--- tuple.py
+++ tuple.py
@@ -3,3 +3,7 @@ tup = (21,26,36,25,1,3,3,3,5)
 a=tup.count(3)
 print(a)
 #it is ordered immutable collection of elemts
+print(id(a))
+b=a
+print(id(b))
+print(id(3))
---
+++ while1.py
@@ -0,0 +1,28 @@
+
+
+# i =1
+# while i<=5:
+#     print(f"{i}  hello")
+#     i+=1
+# out:       1 2 3 4 5 
+
+
+
+i=5
+while i>=1:
+    print(f" {i} hello")
+    i-=1
+# out 5 4 3 2 1 
+
+print("***********************************\n")
+
+i=1
+while i<=5:
+    print("  hello ",end='')
+    j=1
+    while j<=4:
+        print(" daa ", end='')
+        j+=1
+    i+=1
+    print()
+    
